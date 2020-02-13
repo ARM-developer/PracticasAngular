71818d40afcf41e05185feb81e8a86b7e20ebed0
Alejandro Ruiz <angel_alexito123@hotmail.com>
2020-02-13 12:16:47 -0600

    directivas ng en angular

--- src/app/videojuego/videojuego.component.ts
+++ src/app/videojuego/videojuego.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, DoCheck, OnDestroy} from "@angular/core";
 /*Component, es el metodo que genera el componente
 oninit interfaz que obliga a crear el metodo ngOnInit el cual sera el primero en usarse despues del constructor "se sabe que el constructor es lo primero a ejecutarse en una clase"
-*/
+DoCheck cada vez que se produce un cambio en los componentes de la aplicacion,
+OnDestroy cada vez que se elimina un componente*/ 
 
 @Component({
     selector: "videojuego",
     templateUrl: './videojuego.component.html' 
 })
-export class VideojuegosComponent {
+export class VideojuegosComponent implements OnInit, DoCheck, OnDestroy{
     public titulo : string;
     public listado: string;
 
     constructor(){
         this.titulo = 'Componente de Videojuegos';
         this.listado = 'los mejores videojuegos del mundo';
-        console.log('se ha cargado');        
+       // console.log('se ha cargado');        
     }
 
     ngOnInit(){
-        console.log('OnInit ejecutado');        
+       // console.log('OnInit ejecutado');        
+    }
+
+    ngDoCheck(){
+       // console.log('DoCheck ejecutado');        
+    }
+
+    ngOnDestroy(){
+       // console.log('OnDestroy ejecutado');        
+    }
+
+    cambiartitulo(titte:string): void{
+       // this.titulo = titte;
     }
 }
--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -1,17 +1,60 @@
-import { Component } from "@angular/core";
+import { Component, OnInit} from "@angular/core";
+import {Zapatilla} from "../models/zapatilla"
 
 @Component({
     selector: "zapatillas",
     templateUrl: './zapatillas.component.html' 
 })
-export class ZapatillasComponent {
+export class ZapatillasComponent implements OnInit{
     public titulo : string;
-    public listado: string;
+    public zapatillas: Array<Zapatilla>;
+    public marcas:string[]; 
+    public color:string;
+    public mi_marca: string;
 
     constructor(){
-        this.titulo = 'Componente de zapatillas';
-        this.listado = 'las mayores mascar de zapatillas en el mundo';
+        this.mi_marca = "fila";
+        this.color = 'red';
+        this.marcas = new Array();
+        this.zapatillas = [
+            new Zapatilla('Nemesis', 'Adidas', 'white', 3600, true),
+            new Zapatilla('Mercury', 'Nike', 'Black/White', 5800, true),
+            new Zapatilla('Brazil', 'Pirma', 'blue', 2800, false),   
+            new Zapatilla('Yezz', 'Adidas', 'white', 2200, true),
+            new Zapatilla('Air Max', 'Nike', 'Black/White', 3600, true),
+            new Zapatilla('React Element', 'Nike', 'Blanco/Azul', 2000, true)
+        ];
+    }
 
-        console.log('se ha cargado');        
+    ngOnInit(){
+        console.log(this.zapatillas);
+        this.getMarcas();
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) => {
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);  
+            }
+            //console.log(index);            
+        });
+        
+        console.log(this.marcas);        
+    }
+
+    getMarca(){
+        alert(this.mi_marca);
+    }
+
+    addMarca(){
+        this.marcas.push(this.mi_marca);
+    }
+
+    borrarMarca(indice){
+        this.marcas.splice(indice,1);
+    }
+
+    onBlur(){
+        console.log('has salido del blur');        
     }
 }
