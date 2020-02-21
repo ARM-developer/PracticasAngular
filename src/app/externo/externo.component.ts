1a11ab1349bc211b4f485b40378ed29f9de3971a
Alejandro Ruiz <angel_alexito123@hotmail.com>
2020-02-21 09:34:37 -0600

    angular y servicio ajax

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -12,12 +12,17 @@ export class ExternoComponent implements OnInit {
   public user :any;
   public userId : any;
   public fecha: any;
+  public new_user : any;
+  public usuario_guardado;
 
   constructor(
     private _peticionesService : PeticionesService
   ) {
     this.userId = 1;
-    this.fecha  = new Date(2020, 0, 21);
+    this.new_user  = {
+                      "name": "",
+                      "job": ""
+                    };
    }
 
   ngOnInit(): void {
@@ -32,6 +37,18 @@ export class ExternoComponent implements OnInit {
       },
       error =>{
         console.log(<any>error);
+      }
+    );
+  }
+
+  onSubmit(form){
+    this._peticionesService.addUser(this.new_user).subscribe(
+      response => {
+        this.usuario_guardado = response;     
+        form.reset();   
+      },
+      error => {
+        console.log(<any>error);        
       }
     );
   }
